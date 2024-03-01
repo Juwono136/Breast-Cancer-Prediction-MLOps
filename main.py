3fd7ee33f3ecc0eb36925cf99731d3a5cd7c8849
Juwono <unosega@gmail.com>
2024-03-01 19:20:05 +0700

    utils added

---
+++ main.py
@@ -0,0 +1,3 @@
+from breastCancer import logger
+
+logger.info("Welcome to this project!")
--- template.py
+++ template.py
@@ -32,12 +32,13 @@ for filepath in list_of_files:
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info("Creating directory; %s for the file: %s", filedir, filename)
+        logging.info("Creating directory; %s for the file: %s",
+                     filedir, filename)
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w", encoding="utf-8") as f:
             pass
-            logging.info("Creating empty file: %s", filepath)
+        logging.info("Creating empty file: %s", filepath)
 
     else:
         logging.info("%s is already exists", filename)
