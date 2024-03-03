a019829441e81b345c5967ccddcc8d2e39aae96b
Juwono <unosega@gmail.com>
2024-03-03 16:49:07 +0700

    data validation added

--- src/breastCancer/config/configuration.py
+++ src/breastCancer/config/configuration.py
@@ -1,16 +1,19 @@
 from breastCancer.constants import *
 from breastCancer.utils.common import read_yaml, create_directories
-from breastCancer.entity.config_entity import DataIngestionConfig
+from breastCancer.entity.config_entity import (
+    DataIngestionConfig, DataValidationConfig)
 
 
 class ConfigurationManager:
     def __init__(
             self,
             config_filepath=CONFIG_FILE_PATH,
-            params_filepath=PARAMS_FILE_PATH):
+            params_filepath=PARAMS_FILE_PATH,
+            schema_filepath=SCHEMA_FILE_PATH):
 
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
 
         create_directories([self.config.artifacts_root])
 
@@ -26,3 +29,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            data_dir=config.data_dir,
+            status_file=config.status_file,
+            all_schema=schema
+        )
+
+        return data_validation_config
--- src/breastCancer/entity/config_entity.py
+++ src/breastCancer/entity/config_entity.py
@@ -7,3 +7,11 @@ class DataIngestionConfig:
     root_dir: Path
     source_data: Path
     local_data_file: Path
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    data_dir: Path
+    status_file: str
+    all_schema: dict
